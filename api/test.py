00ae0e2f8823b424f0c57cc27c2dc131b0280f8a
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-07-18 23:30:56 +0530

    Cleaned Code

--- api/test.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, Response
-app = Flask(__name__)
-
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-@app.route('/api/test')
-def catch_all():
-    return Response("<h1>Flask</h1><p>You visited:</p>", mimetype="text/html")
