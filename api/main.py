e313ce87d2e465a030f1be99104879edb7357a94
Vaibhav Chanana <vaibhavchanana@yahoo.co.in>
2020-07-18 22:42:15 +0530

    Uncommented GIF Code

--- api/main.py
+++ api/main.py
@@ -10,26 +10,26 @@ CURSOR_WIDTH = 5
 CURSOR_HEIGHT = FONT_SIZE
 
 def createBanner(stream: io.BytesIO):
-    # text = "Hi, I am Vaibhav."
-    # gif = []
-    # pixel = [10, 10]
-    # fnt = ImageFont.truetype("fonts/Roboto-Regular.ttf", FONT_SIZE)
+    text = "Hi, I am Vaibhav."
+    gif = []
+    pixel = [10, 10]
+    fnt = ImageFont.truetype("fonts/Roboto-Regular.ttf", FONT_SIZE)
 
-    # prev_im = Image.new("RGB", (700, 150), (255,255,255))
+    prev_im = Image.new("RGB", (700, 150), (255,255,255))
 
-    # for i in range(len(text)):
-    #     temp_im = prev_im
-    #     d = ImageDraw.Draw(temp_im)
-    #     d.text(tuple(pixel), text[i], font=fnt, fill=(0, 0, 0))
-    #     size = d.textsize(text[i], font=fnt)
-    #     pixel[0] += size[0]
-    #     prev_im = temp_im.copy()
-    #     d.rectangle([pixel[0], pixel[1], pixel[0]+CURSOR_WIDTH, pixel[1]+CURSOR_HEIGHT], fill=(0,0,0))
-    #     gif.append(temp_im)
+    for i in range(len(text)):
+        temp_im = prev_im
+        d = ImageDraw.Draw(temp_im)
+        d.text(tuple(pixel), text[i], font=fnt, fill=(0, 0, 0))
+        size = d.textsize(text[i], font=fnt)
+        pixel[0] += size[0]
+        prev_im = temp_im.copy()
+        d.rectangle([pixel[0], pixel[1], pixel[0]+CURSOR_WIDTH, pixel[1]+CURSOR_HEIGHT], fill=(0,0,0))
+        gif.append(temp_im)
     
-    # gif[0].save(stream, format="GIF", save_all=True, append_images=gif[1:], duration=100)
-    im = Image.open("static/stock.jpg")
-    im.save(stream, format="JPEG")
+    gif[0].save(stream, format="GIF", save_all=True, append_images=gif[1:], duration=100)
+    # im = Image.open("static/stock.jpg")
+    # im.save(stream, format="JPEG")
     return
 
 @app.route('/api/main')
@@ -38,6 +38,6 @@ def serve_image():
     try:
         createBanner(stream)
         stream.seek(0) # HACK
-        return send_file(stream, mimetype="image/jpeg")
+        return send_file(stream, mimetype="image/gif")
     except Exception as e:
         return str(e)
